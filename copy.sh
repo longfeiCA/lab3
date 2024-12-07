10c5ed2cc0e26547d81188c5a4e6634516c80160
Longfei Jiao <ljiao@ualberta.ca>
2024-12-06 17:23:51 -0700

    remove test5 which does not exist

--- copy.sh
+++ copy.sh
@@ -2,6 +2,6 @@
 
 file_to_copy="fs"
 
-for dir in tests/test1 tests/test2 tests/test3 tests/test4 tests/test5; do
+for dir in tests/test1 tests/test2 tests/test3 tests/test4; do
     cp "$file_to_copy" "$dir"
 done
